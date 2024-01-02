719db3990a4854a1217bafa5d69f6ac2dcfec2e8
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-01-02 16:48:21 +0100

    Added customer creation

--- BookingApp/BookingApp/Helpers/AddData.cs
+++ BookingApp/BookingApp/Helpers/AddData.cs
@@ -26,5 +26,70 @@ namespace BookingApp.Helpers
                 dbContext.SaveChanges();
             }
         }
+
+        public static void AddCustomer()
+        {
+            Console.WriteLine("Please enter your first name");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your e-mail");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Please enter your username");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Please enter your pasword");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Please enter your address");
+            string address = Console.ReadLine();
+
+            bool isBusinessCustomer = false;
+            bool isValidInput = false;
+
+            while (!isValidInput)
+            {
+                Console.WriteLine("Are you a business customer? (y/n)");
+                string businessCustomer = Console.ReadLine().ToLower();
+
+                if (businessCustomer == "y")
+                {
+                    isBusinessCustomer = true;
+                    isValidInput = true;
+                }
+                else if (businessCustomer == "n")
+                {
+                    isBusinessCustomer = false;
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input, please enter 'Y' or 'N'");
+                }
+            }
+
+
+            using (var dbContext = new BookingsContext())
+            {
+                var newCustomer = new Customer
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    UserName = username,
+                    Password = password,
+                    Address = address,
+                    IsBusinessCustomer = isBusinessCustomer
+                };
+                dbContext.Customers.Add(newCustomer);
+                dbContext.SaveChanges();
+
+                Console.WriteLine("Thank you, your customer account has been created!");
+                Console.ReadLine();
+            }
+        }
     }
 }
--- BookingApp/BookingApp/Program.cs
+++ BookingApp/BookingApp/Program.cs
@@ -1,23 +1,44 @@
-﻿using BookingApp.Models;
-
-namespace BookingApp
+﻿namespace BookingApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            using (var dbContext = new BookingsContext())
-            {
-                var week = dbContext.Weeks.FirstOrDefault(w => w.Id == 1);
-                Console.WriteLine($"Monday:   {week.Monday.PadLeft(10)}");
-                Console.WriteLine($"Tuesday:  {week.Tuesday.PadLeft(10)}");
-                Console.WriteLine($"Wednesday:{week.Wednesday.PadLeft(10)}");
-                Console.WriteLine($"Thursday: {week.Thursday.PadLeft(10)}");
-                Console.WriteLine($"Friday:   {week.Friday.PadLeft(10)}");
-                Console.WriteLine($"Saturday: {week.Saturday.PadLeft(10)}");
-                Console.WriteLine($"Sunday:   {week.Sunday.PadLeft(10)}");
-                Console.WriteLine("Test");
+            //using (var dbContext = new BookingsContext())
+            //{
+            //    var week = dbContext.Weeks.FirstOrDefault(w => w.Id == 1);
+            //    Console.WriteLine($"Monday:   {week.Monday.PadLeft(10)}");
+            //    Console.WriteLine($"Tuesday:  {week.Tuesday.PadLeft(10)}");
+            //    Console.WriteLine($"Wednesday:{week.Wednesday.PadLeft(10)}");
+            //    Console.WriteLine($"Thursday: {week.Thursday.PadLeft(10)}");
+            //    Console.WriteLine($"Friday:   {week.Friday.PadLeft(10)}");
+            //    Console.WriteLine($"Saturday: {week.Saturday.PadLeft(10)}");
+            //    Console.WriteLine($"Sunday:   {week.Sunday.PadLeft(10)}");
+            //    Console.WriteLine("Test");
 
+            //}
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Duchene Congress Center booking portal\n");
+                Console.WriteLine("Please make a choice in the menu below");
+                Console.WriteLine("[1]. Sign in as admin");
+                Console.WriteLine("[2]. Sign in as customer");
+                Console.WriteLine("[3]. Create a customer account");
+
+                int menuChoice;
+                bool success = int.TryParse(Console.ReadLine(), out menuChoice);
+
+                switch (menuChoice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        Helpers.AddData.AddCustomer();
+                        break;
+                }
             }
         }
     }
