3974e9586c83cc5abc7a9a36e3d0c742754da4eb
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-01-02 16:50:47 +0100

    changed Username to UserName in Administrator

--- BookingApp/BookingApp/Migrations/20240102154955_changed Username to UserName in Administrator class.Designer.cs
+++ BookingApp/BookingApp/Migrations/20240102154955_changed Username to UserName in Administrator class.Designer.cs
@@ -3,6 +3,7 @@ using BookingApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingApp.Migrations
 {
     [DbContext(typeof(BookingsContext))]
-    partial class BookingsContextModelSnapshot : ModelSnapshot
+    [Migration("20240102154955_changed Username to UserName in Administrator class")]
+    partial class changedUsernametoUserNameinAdministratorclass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookingApp/BookingApp/Migrations/20240102154955_changed Username to UserName in Administrator class.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedUsernametoUserNameinAdministratorclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Administrators",
+                newName: "UserName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Administrators",
+                newName: "Username");
+        }
+    }
+}
